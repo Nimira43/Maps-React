c92c3acb20c24a0894f0be9f9ba0eb5bdbb56741
Nimira43 <n.rabone72@aol.com>
2024-11-04 21:39:36 +0000

    Creates form in LocationSearch to handle submissions

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -6,9 +6,23 @@ interface LocationSearchProps {
 }
 
 export default function LocationSearch({ onPlaceClick }: LocationSearchProps ) {
+  const [term, setTerm] = useState('')
   const [places, setPlaces] = useState<Place[]>([])
   
   return (
-    <div>LocationSearch</div>
+    <div>
+      <form>
+        <label className='font-semi-bold text-[#eee]' htmlFor='term'>
+          Search
+        </label>
+        <input 
+          type="text" 
+          className='border border-[#eee] bg-[#111] text-[#ff4500] rounded-sm focus:border-[#ff4500] px-4 py-2 w-full'
+          id='term'
+          value={term}
+          onChange={e => setTerm(e.target.value)}  
+        />
+      </form>
+    </div>
   )
 }
