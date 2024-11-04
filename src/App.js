ee6d848d080bb2b8bd6fa047a102edaf67fbc43e
Nimira43 <n.rabone72@aol.com>
2024-11-04 20:17:33 +0000

    Cleaned boilerplate up. Brought in font and favicon. Set up CSS resets and button styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Maps React</h1>
+      <button className='btn btn-orange'>Orange</button>
+      <button className='btn btn-dark'>Dark</button>
+      <button className='btn btn-light'>Light</button>
+      <button className='btn btn-grey'>Grey</button>
+      <button className='btn btn-sky'>Sky</button>
     </div>
-  );
+  )
 }
 
-export default App;
